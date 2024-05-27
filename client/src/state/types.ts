6becf2b205786976a4ea2531af8fa9418f5f0df5
felixoder <debayanghosh408@gmail.com>
2024-05-28 01:17:28 +0530

    This is initial commit

---
+++ client/src/state/types.ts
@@ -0,0 +1,60 @@
+export interface ExpensesByCategory {
+  salaries: number;
+  supplies: number;
+  services: number;
+}
+export interface Month {
+  id: string;
+  month: string;
+
+  revenue: number;
+  expenses: number;
+  nonOperationalExpenses: number;
+  operationalExpenses: number;
+}
+export interface Day {
+  id: string;
+  date: string;
+  expenses: number;
+  revenue: number;
+}
+
+export interface GetKpisResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface GetProductsResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  price: number;
+  expense: number;
+
+  transactions: Array<string>;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface GetTransactionsResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  buyer: string;
+  expense: number;
+  amount: number;
+  productId: Array<string>;
+  createdAt: string;
+  updatedAt: string;
+}
+
